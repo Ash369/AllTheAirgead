b76c0463a904041a8e7011604e532afb838c055c
freakyredhead91 <freakyredhead91@hotmail.com>
2014-11-14 00:27:44 +0000

    Created new app based on C# to use async features. Copied old template to new app. Implemented login and register feature.

---
+++ AllTheAirgeadApp/alltheairgeadApp/Handlers/ZumoAuthHeaderHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using alltheairgeadApp.Services;
+
+namespace alltheairgeadApp.Handlers
+{
+    public class ZumoAuthHeaderHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(CustomAccountService.AuthenticationToken))
+            {
+                throw new InvalidOperationException("User is not currently logged in");
+            }
+
+            request.Headers.Add("X-ZUMO-AUTH", CustomAccountService.AuthenticationToken);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ AllTheAirgeadApp/alltheairgeadApp/Services/CustomAccountService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.UI.Popups;
+using Newtonsoft.Json.Linq;
+using Microsoft.WindowsAzure.MobileServices;
+using alltheairgeadApp.Handlers;
+using alltheairgeadApp.DataObjects;
+
+namespace alltheairgeadApp.Services
+{
+    public class CustomAccountService
+    {
+        public static string AuthenticationToken { get; set; }
+
+        public async Task<Boolean> Register(string username, string password)
+        {
+            string message;
+            Boolean result;
+            try
+            {
+                string HttpRequestBodyString = "{username:\"" + username + "\", password:\"" + password + "\"}";
+                StringContent HttpRequestBody = new StringContent(HttpRequestBodyString, Encoding.UTF8, "application/json");
+                var response = await App.alltheairgeadClient.InvokeApiAsync("CustomRegistration", HttpRequestBody, System.Net.Http.HttpMethod.Post, null, null);
+                response.EnsureSuccessStatusCode();
+
+                if(! await Login(username, password))
+                {
+                    message = "Registered but could not log in";
+                    result = true;
+                }
+                message = "Registered and Logged in successfully";
+                result = true;
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                message = ex.Message;
+                result = false;
+            }
+            MessageDialog Dialog = new MessageDialog(message);
+            await Dialog.ShowAsync();
+            return result;
+        }
+
+        public async Task<Boolean> Login(string username, string password)
+        {
+            string message;
+            Boolean result;
+            try
+            {
+                string HttpRequestBodyString = "{username:\"" + username + "\", password:\"" + password + "\"}";
+                StringContent HttpRequestBody = new StringContent(HttpRequestBodyString, Encoding.UTF8, "application/json");
+                var response = await App.alltheairgeadClient.InvokeApiAsync("CustomLogin", HttpRequestBody, System.Net.Http.HttpMethod.Post, null, null);
+                response.EnsureSuccessStatusCode();
+
+                string jsonString = await response.Content.ReadAsStringAsync();
+                JObject jsonObject = JObject.Parse(jsonString);
+                string token = jsonObject["authenticationToken"].ToString();
+                CustomAccountService.AuthenticationToken = token;
+
+                message = "Login Successfull";
+                result = true;
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                message = ex.Message;
+                result = false;
+            }
+            MessageDialog Dialog = new MessageDialog(message);
+            await Dialog.ShowAsync();
+            return result;
+        }
+    }
+}
